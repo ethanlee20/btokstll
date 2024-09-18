a6606651b818daba5b0720c103e3d5a057fe8a9c
ethanlee20 <elee20@hawaii.edu>
2024-09-18 12:06:27 -1000

    calc vars script

---
+++ logic/scripts/calc_vars.py
@@ -0,0 +1,8 @@
+
+from helpers.decay_variables import calculate_variables
+
+
+ell = "mu"
+data_dir = "/home/belle2/elee20/btokstll/state/new_physics/data/raw/bkg/mu_sideb_generic_mix_test/sub00"
+
+calculate_variables(ell, data_dir, data_dir)
