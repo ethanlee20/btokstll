37e3551d59dfac91c54b460a863e5e2203fdbfff
ethanlee20 <elee20@hawaii.edu>
2025-09-02 09:59:59 -1000

    move to new folder

---
+++ common/logic/scripts/helpers/datasets/make_and_save/aggregated_background.py
@@ -0,0 +1,68 @@
+
+from pathlib import Path
+
+from pandas import read_pickle, read_parquet
+
+from ..constants import Names_of_Variables
+
+
+def make_path_to_raw_bkg_file(
+    path_to_raw_bkg_dir,
+    charge_or_mix,
+    split
+):
+    
+    def make_name_of_raw_bkg_file(charge_or_mix, split):
+        if charge_or_mix not in ("charge", "mix"):
+            raise ValueError
+        # name = f"mu_sideb_generic_{charge_or_mix}_{split}.pkl"
+        name = f"{charge_or_mix}_sb_bkg_{split}.parquet"
+        return name
+    
+    file_name = make_name_of_raw_bkg_file(
+        charge_or_mix=charge_or_mix, 
+        split=split
+    )
+
+    path = Path(path_to_raw_bkg_dir).joinpath("new").joinpath(file_name)
+    return path
+
+
+def load_raw_background_file(
+    path_to_raw_bkg_dir,
+    charge_or_mix,
+    split,
+    verbose=True,    
+):
+    
+    file_path = make_path_to_raw_bkg_file(
+        path_to_raw_bkg_dir=path_to_raw_bkg_dir,
+        charge_or_mix=charge_or_mix,
+        split=split
+    )
+
+    dataframe = read_parquet(file_path)[Names_of_Variables().list_] 
+    if verbose:
+        print(f"Loaded raw background file: {file_path}")   
+    return dataframe
+
+
+def load_raw_charge_and_mix_background_files(
+    path_to_raw_bkg_dir,
+    split,
+    verbose=True
+):
+    
+    charge_dataframe = load_raw_background_file(
+        path_to_raw_bkg_dir=path_to_raw_bkg_dir,
+        charge_or_mix="charge",
+        split=split,
+        verbose=verbose
+    )
+    mix_dataframe = load_raw_background_file(
+        path_to_raw_bkg_dir=path_to_raw_bkg_dir,
+        charge_or_mix="mix",
+        split=split,
+        verbose=verbose
+    )
+    return charge_dataframe, mix_dataframe
